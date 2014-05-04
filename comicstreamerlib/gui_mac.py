ebbfa7dc3af5aa26e04a7b505d8ecaaff6cf082c
beville <comictagger@gmail.com>
2014-05-04 13:20:31 -0700

    some takes on sys tray/notification GUI

---
+++ comicstreamerlib/gui_mac.py
@@ -0,0 +1,74 @@
+from libs.rumps import rumps
+import time
+from PyObjCTools import AppHelper
+import webbrowser
+import os
+
+from config import ComicStreamerConfig
+
+rumps.debug_mode(True)  # turn on command line logging information for development - default is off
+
+class MacGui(rumps.App):
+    def __init__(self, apiServer):
+        super(MacGui, self).__init__("ComicStreamer", icon=os.path.join(ComicStreamerConfig.baseDir(),"images", "trout.png"))
+        self.apiServer =  apiServer
+        
+        self.menu = [
+            #rumps.MenuItem('About'), 
+            'Show ComicStreamer UI',
+            #None,  # None functions as a separator in your menu
+            #{'Arbitrary':
+            #    {"Depth": ["Menus", "It's pretty easy"],
+            #     "And doesn't": ["Even look like Objective C", rumps.MenuItem("One bit", callback=self.onebitcallback)]}},
+            None
+        ]         
+    
+    #@rumps.clicked("About")
+    #def about(self, sender):
+    #    #sender.title = 'NOM' if sender.title == 'About' else 'About'  # can adjust titles of menu items dynamically
+    #    rumps.alert("ComicStreamer")
+
+    @rumps.clicked("Show ComicStreamer UI")
+    def about(self, sender):
+        webbrowser.open("http://localhost:{0}".format(self.apiServer.port), new=0)
+        
+    @rumps.clicked("Quit")
+    def about(self, sender):
+        #rumps.alert("My quit message")
+        self.apiServer.shutdown()
+        AppHelper.stopEventLoop()
+        print "after stop"
+        
+    
+    #@rumps.clicked("Arbitrary", "Depth", "It's pretty easy")  # very simple to access nested menu items
+    #def does_something(self, sender):
+    #    my_data = {'poop': 88}
+    #    rumps.notification(title='Hi', subtitle='There.', message='Friend!',  data=my_data)
+    
+    
+    #@rumps.clicked("Preferences")
+    #def not_actually_prefs(self, sender):
+    #    if not sender.icon:
+    #        sender.icon = 'level_4.png'
+    #    sender.state = not sender.state
+    
+    
+    #@rumps.timer(4)  # create a new thread that calls the decorated function every 4 seconds
+    #def write_unix_time(self, sender):
+    #    with self.rumps_app.open('times', 'a') as f:  # this opens files in your app's Application Support folder
+    #        f.write('The unix time now: {}\n'.format(time.time()))
+    
+    
+    #@rumps.clicked("Arbitrary")
+    #def change_statusbar_title(self, sender):
+    #    app.title = 'Hello World' if self.rumps_app.title != 'Hello World' else 'World, Hello'
+    
+    
+    @rumps.notifications
+    def notifications(self, notification):  # function that reacts to incoming notification dicts
+        print notification
+    
+    
+    def onebitcallback(self, sender):  # functions don't have to be decorated to serve as callbacks for buttons
+        print 4848484            # this function is specified as a callback when creating a MenuItem below
+    
---
+++ comicstreamerlib/gui_qt.py
@@ -0,0 +1,44 @@
+import sys
+import webbrowser
+import os
+from config import ComicStreamerConfig
+
+from PyQt4 import QtGui,QtCore
+
+class SystemTrayIcon(QtGui.QSystemTrayIcon):
+    def __init__(self, icon, app):
+        QtGui.QSystemTrayIcon.__init__(self, icon, None)
+        self.app = app
+        self.menu = QtGui.QMenu(None)
+        exitAction = self.menu.addAction("Exit")
+        self.setContextMenu(self.menu)
+        exitAction.triggered.connect( self.quit )
+
+    def quit(self):
+
+        QtCore.QCoreApplication.quit()
+        
+
+class QtBasedGui():
+    def __init__(self, apiServer):
+        self.apiServer =  apiServer
+        
+        self.app = QtGui.QApplication(sys.argv)
+        
+        pixmap = QtGui.QPixmap(os.path.join(ComicStreamerConfig.baseDir(),"images", "trout.png"))
+        icon = QtGui.QIcon( pixmap.scaled(16,16))       
+
+        self.trayIcon = SystemTrayIcon(icon,self)
+    
+        self.trayIcon.show() 
+
+    def run(self):
+        try:
+            self.app.exec_()
+        except KeyboardInterrupt:
+            pass
+
+
+
+if __name__ == '__main__':
+    QtGui().run()
