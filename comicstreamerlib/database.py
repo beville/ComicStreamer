6fc6b4baa33fd97f5b5a0aec46dd9dcc673a9fde
beville <comictagger@gmail.com>
2014-05-07 00:26:01 -0700

    handle an unexpected condition

--- comicstreamerlib/database.py
+++ comicstreamerlib/database.py
@@ -217,9 +217,10 @@ class Comic(Base):
         out_dict = {}
         # iterate over the list of credits mini dicts:
         for c in self.credits_raw:
-            if not out_dict.has_key(c.role.name):
-                out_dict[c.role.name] = []
-            out_dict[c.role.name].append(c.person.name)
+            if c.role:
+                if not out_dict.has_key(c.role.name):
+                    out_dict[c.role.name] = []
+                out_dict[c.role.name].append(c.person.name)
         
         return out_dict
 
